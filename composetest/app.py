dc85e015cc64f912370f881a4a0507dd62abc74c
yehor-vynokurov <41213534+yehor-vynokurov@users.noreply.github.com>
2018-12-03 11:49:47 +0100

    Add files via upload

---
+++ composetest/app.py
@@ -0,0 +1,27 @@
+import redis
+from flask import Flask
+
+
+app = Flask(__name__)
+cache = redis.Redis(host='redis', port=6379)
+
+
+def get_hit_count():
+    retries = 5
+    while True:
+        try:
+            return cache.incr('hits')
+        except redis.exceptions.ConnectionError as exc:
+            if retries == 0:
+                raise exc
+            retries -= 1
+            time.sleep(0.5)
+
+
+@app.route('/')
+def hello():
+    count = get_hit_count()
+    return 'Dzien dobry. Jestes tutaj {} raz.\n'.format(count)
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", debug=True)
